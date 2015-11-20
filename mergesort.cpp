a3939d7cbef5028fd944b096253232a8dac1fb21
zakrywilson <zakrywilson@gmail.com>
2015-11-19 19:16:38 -0700

    Implemented multithreading (with 2 threads)

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,26 +1,77 @@
 /*
  * Zach Wilson
+ * Multithreading Merge Sort
  */
 
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <pthread.h>
 
-void merge(int *, int *, int, int, int);
+#define STRUCT_LOADER( x, n, s, l, h ) x.numbers = n, x.size = s, x.low = l, x.high = h
+#define STRUCT_LOADER_POINTER( x, n, s, l, h ) x->numbers = n, x->size = s, x->low = l, x->high = h
 
-void merge_sort(int *numbers, int *temp, int low, int high) {
-  int mid;
-  if (low < high) {
-    mid = (low + high)/2;
-    merge_sort(numbers, temp, low, mid);
-    merge_sort(numbers, temp, mid + 1, high);
-    merge(numbers, temp, low, mid, high);
-  }
+
+void merge(int *, int, int, int, int);
+
+void* merge_sort(void* p);
+
+struct sort_args {
+  int *numbers;
+  int size;
+  int low;
+  int high;
+};
+
+void merge_sort_start(int *numbers, int size, int low, int high) {
+
+  int mid = (low + high)/2;
+  struct sort_args left, right;
+
+  pthread_t thread_left, thread_right;
+
+  STRUCT_LOADER(left, numbers, size, low, mid);
+  STRUCT_LOADER(right, numbers, size, mid + 1, high);
+
+  pthread_create(&thread_left, NULL, &merge_sort, (void *) &left);
+  pthread_create(&thread_right, NULL, &merge_sort, (void *) &right);
+
+  pthread_join(thread_left, NULL);
+  pthread_join(thread_right, NULL);
+
+  merge(numbers, size, low, mid, high);
 }
 
-void merge(int *numbers, int *temp, int low, int mid, int high) {
+void* merge_sort(void* p) {
+  struct sort_args *args = (struct sort_args *) p;
+  int mid;
+  int low_save = args->low;
+  int high_save = args->high;
+
+  if (args->low < args->high) {
+    mid = (args->low + args->high) / 2;
+
+    STRUCT_LOADER_POINTER(args, args->numbers, args->size, args->low, mid);
+    merge_sort((void *) args);
+
+    STRUCT_LOADER_POINTER(args, args->numbers, args->size, mid + 1, high_save);
+    merge_sort((void *) args);
+
+    merge(args->numbers, args->size, low_save, mid, high_save);
+  }
+
+  return NULL;
+}
+
+void merge(int *numbers, int size, int low, int mid, int high) {
   int h, i, j, k;
   h = low;
   i = low;
   j = mid + 1;
+
+  int temp[size];
 
   // Traverse both halves of the array
   while ((h <= mid) && (j <= high)) { 
@@ -29,11 +80,11 @@ void merge(int *numbers, int *temp, int low, int mid, int high) {
     // less than element of right half
     if (numbers[h] <= numbers[j]) {
 
-      // store element of left half in the temporary array
+      // store element of left half in the temp array
       temp[i] = numbers[h]; 
 
       // shift the index of the array 
-      // from which the element was copied to temporary 
+      // from which the element was copied to temp 
       h++;
     }
 
@@ -44,14 +95,14 @@ void merge(int *numbers, int *temp, int low, int mid, int high) {
       temp[i] = numbers[j];
 
       // shift the index of the array from 
-      // which the element was copied to temporary 
+      // which the element was copied to temp 
       j++; 
     }
     i++;
   }
 
   // if traversal of left half is done, 
-  // copy remaining elements from right half to temporary
+  // copy remaining elements from right half to temp
   if (h > mid) {
     for (k = j; k <= high; k++) {
       temp[i] = numbers[k];
@@ -59,7 +110,7 @@ void merge(int *numbers, int *temp, int low, int mid, int high) {
     }
   }
 
-  // otherwise copy remaining elements from left half to temporary
+  // otherwise copy remaining elements from left half to temp
   else {
     for (k = h; k <= mid; k++) {
       temp[i] = numbers[k];
@@ -67,22 +118,28 @@ void merge(int *numbers, int *temp, int low, int mid, int high) {
     }
   }
 
-  // recopy the values from temporary to original array.
+  // recopy the values from temp to original array.
   for (k = low; k <= high; k++) 
     numbers[k] = temp[k];
 }
 
 int main() {
+
   int numbers[] = {12, 10, 43, 23, -78, 45, 123, 56, 98, 41, 90, 24};
   int num;
 
-  num = sizeof(numbers)/sizeof(int);
+  num = sizeof(numbers) / sizeof(int);
+
+  std::cout << "Unsorted: ";
+  for (int i = 0; i < num; i++)
+    std::cout << numbers[i] << " ";
 
   // temporary array to be used for merging
   int temp[num]; 
 
-  merge_sort(numbers, temp, 0, num - 1);
+  merge_sort_start(numbers, num, 0, num - 1);
 
+  std::cout << std:: endl << "Sorted: ";
   for (int i = 0; i < num; i++)
     std::cout << numbers[i] << " ";
 
